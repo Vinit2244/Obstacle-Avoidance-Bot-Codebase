a152879366291fdac1cf0a82542a96f7b78c8c39
Vinit Mehta <vinit2004mehtaa@gmail.com>
2023-12-01 20:49:32 +0530

    done

--- esw/esw.ino
+++ esw/esw.ino
@@ -25,33 +25,30 @@
 #include <PubSubClient.h>
 #include "BluetoothSerial.h"
 
-// #if !defined(CONFIG_BT_ENABLED) || !defined(CONFIG_BLUEDROID_ENABLED)
-// #error Bluetooth is not enabled! Please run `make menuconfig` to and enable it
-// #endif
-
 BluetoothSerial SerialBT;
 
 char t;
 int BT_on_off_status = 0;
+char mode = 'a';  // In auto mode by default
 
-#define Shinde_ssid "hello.c"
+#define Shinde_ssid     "Hello.c"
 #define Shinde_password "pwla1953"
-#define Samyak_ssid "Lorem ipsum"
+#define Samyak_ssid     "Lorem ipsum"
 #define Samyak_password "Getlostyoutrespassers"
 
-#define ssid Samyak_ssid
-#define password Samyak_password
-#define server "mqtt3.thingspeak.com"
+#define ssid         Shinde_ssid
+#define password     Shinde_password
+#define server       "mqtt3.thingspeak.com"
 #define MQTTUsername "Hi81BiQeBgcQLS8LGTgRKTc"
 #define MQTTClientID "Hi81BiQeBgcQLS8LGTgRKTc"
-#define MQTTPass "QLTffZasuHLK+fZiB64op7dL"
-#define channelID 2289111
-#define WriteAPIKey "4X3O813MKA5OQ8A2"
-#define port 1883
-#define HIGH_T 150 // in miliseconds
-#define LOW_T 20 // in miliseconds
-#define maxSpeed 1000
-#define danger 30.0 // cms
+#define MQTTPass     "QLTffZasuHLK+fZiB64op7dL"
+#define channelID    2289111
+#define WriteAPIKey  "4X3O813MKA5OQ8A2"
+#define port         1883
+#define HIGH_T       150 // in miliseconds
+#define LOW_T        20 // in miliseconds
+#define maxSpeed     1000
+#define danger       30.0 // cms
 
 /* robo functions */
 void set_speed(int leftSpeed, int rightSpeed);
@@ -102,14 +99,7 @@ float take_reading(int trig_pin, int echo_pin)
 
 void stop()
 {
-  if (status == STOP)
-  {
-    // do nothing
-  }
-  else
-  {
-    set_speed(0, 0);
-  }
+  set_speed(0, 0);
 }
 
 void go_left(int turn_time)
@@ -434,10 +424,18 @@ void main_loop()
   decide_what_to_do(result);
 
 }
+
 int on_off_flag = 0;
+
+void keep_going_forward()
+{
+  
+}
+
 void loop()
 {
-  if (SerialBT.available()) {
+  if (SerialBT.available())
+  {
     t = SerialBT.read();
     Serial.println(t);
     if (t == '1')
@@ -448,23 +446,118 @@ void loop()
     {
       BT_on_off_status = 0;
     }
+    else if (t == 'm')
+    {
+      if (BT_on_off_status == 1)
+      {
+        // Do nothing and carry on as usual in auto mode
+      }
+      else
+      {
+        // Change the mode to manual
+        mode = 'm';
+      }
+    }
   }
-  if (BT_on_off_status == 1)
+  if (mode == 'a')
   {
-    main_loop();
-    on_off_flag = 0;
+    // Automatic mode
+    if (BT_on_off_status == 1)
+    {
+      main_loop();
+      on_off_flag = 0;
+    }
+    else
+    {
+      stop();
+      status = STOP;
+
+      if(on_off_flag == 0)
+      {
+        publish(take_reading(left_trigger, left_echo), take_reading(right_trigger, right_echo), take_reading(center_trigger, center_echo));
+        on_off_flag = 1;
+      }
+    }
   }
   else
   {
-    stop();
-    status = STOP;
-    // float left_diagonal_distance = take_reading(left_diagonal_trigger, left_diagonal_echo);
-    // float right_diagonal_distance = take_reading(right_diagonal_trigger, right_diagonal_echo);
-    if(on_off_flag == 0)
+    // Manual mode
+    while (1)
     {
-      publish(take_reading(left_trigger, left_echo), take_reading(right_trigger, right_echo), take_reading(center_trigger, center_echo));
-      on_off_flag = 1;
+      if (SerialBT.available())
+      {
+        t = SerialBT.read();
+        Serial.println(t);
+        if (t == 'a')
+        {
+          stop();
+          status = STOP;
+          mode = 'a';
+          break;
+        }
+        else if (t == 'l')
+        {
+          // turn left
+          set_speed(-maxSpeed, maxSpeed);
+          delay(1279);
+          stop();
+          status = STOP;
+        }
+        else if (t == 'r')
+        {
+          // turn right
+          set_speed(maxSpeed, -maxSpeed);
+          delay(1000);
+          stop();
+          status = STOP;
+        }
+        else if (t == 'f')
+        {
+          // go straight and stop is going to bump
+          while (1)
+          {
+            set_speed(maxSpeed, maxSpeed);
+            // If no input given keep going straight but also keep checking if we can go straight or not if not then just stop
+            float cd = take_reading(center_trigger, center_echo);
+            Serial.println(cd);
+            if (cd < danger)
+            {
+              Serial.println("I am here.");
+              stop();
+              status = STOP;
+              return;
+            }
+            else
+            {
+              delay(100);
+            }
+
+            if (SerialBT.available())
+            {
+              t = SerialBT.read();
+              Serial.println(t);
+
+              if (t == '0')
+              {
+                stop();
+                status = STOP;
+                return;
+              }
+              else if (t == 'a')
+              {
+                stop();
+                status = STOP;
+                mode = 'a';
+                return;
+              }
+            }
+          }
+        }
+        else if (t == '0')
+        {
+          stop();
+        }
+      }
     }
-    
   }
 }
